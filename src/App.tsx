d6422afcd0c0abed90eea7feb5ba11252feef203
Tanmay Sarkar <sarkartanmay393@gmail.com>
2023-12-13 15:06:24 +0530

    feat: Removed server and shifted to Supabase with react frontend only

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+import Layout from "./layout/layout";
+import Homepage from "./pages/Home.page";
+import { AuthModal } from "./pages/Auth.page";
+import { useSupabaseContext } from "./provider/supabase/provider";
+import RoomPage from "./pages/Room.page";
+
+function App() {
+  const { user } = useSupabaseContext();
+
+  return (
+    <Layout>
+      <BrowserRouter>
+        {user?.id === undefined && <Navigate to='/auth' />}
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/auth" element={<AuthModal />} />
+          {/* <Route path="/rooms" element={<RoomPage />} /> */}
+          <Route path="/room/:slug" element={<RoomPage />} />
+        </Routes>
+      </BrowserRouter>
+    </Layout>
+  );
+}
+
+export default App;
