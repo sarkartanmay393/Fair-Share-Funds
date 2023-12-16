e8deb8f7b9cb90ba5976b4243eec095ddda8b312
Tanmay Sarkar <sarkartanmay393@gmail.com>
2023-12-17 03:34:49 +0530

    Fixes: roompage dummy data types

--- src/pages/Room.page.tsx
+++ src/pages/Room.page.tsx
@@ -2,12 +2,10 @@ import React, { useEffect } from "react";
 import { Box } from "@mui/material";
 import { useNavigate, useParams } from "react-router-dom";
 
-import { Room } from "../interfaces";
+import { Transaction, TransactionType } from "../interfaces";
 import InputBar from "../components/Room/Input";
-import RoomUserManager from "./UserManager.page";
 import { Database } from "../utils/supabase/types";
-import { useSupabaseContext } from "../provider/supabase/provider";
-import { useStoreActions, useStoreState } from "../store/typedHooks";
+import { useStoreActions } from "../store/typedHooks";
 import { useSupabaseContext } from "../provider/supabase/provider";
 import DisplayInputs from "../components/Room/DisplayInputs";
 import DisplayHistory from "../components/Room/DisplayHistory";
@@ -24,25 +22,12 @@ const styles = {
 
 export default function RoomPage() {
   const { id } = useParams();
-  const pathname = window.location.pathname;
   
   const { supabase, session } = useSupabaseContext();
   const [data, setData] = React.useState<Database["public"]['Tables']['rooms']['Row']>();
 
   const { setAppbarTitle } = useStoreActions((actions) => actions);
   const navigate = useNavigate();
-
-
-  // useEffect(() => {
-  //   const room = rooms?.find((room) => (room.id === id));
-  //   if (room) {
-  //     setAppbarTitle(room.name!);
-  //     setData(room);
-  //   } else {
-  //     navigate('/');
-  //     return;
-  //   }
-  // }, [])
 
   const fetch = async () => {
     // setIsLoading(true);
@@ -69,27 +54,6 @@ export default function RoomPage() {
       setAppbarTitle('Fair Share Funds');
     };
   }, [])
-
-  // const fetch = async () => {
-  //   setIsLoading(true);
-  //   try {
-  //     const { data, error } = await supabase.from('rooms').select().eq('id', id);
-  //     const dt = data && data[0] as Database["public"]['Tables']['rooms']['Row'];
-  //     if (error?.code !== '201' || (dt && !dt.users_id.includes(user.id))) {
-  //       navigate('/');
-  //       return;
-  //     }
-  //     if (dt) { setData({ ...dt, master_sheet: JSON.stringify(dt.master_sheet) }) }
-  //     setIsLoading(false);
-  //   } catch (e) { }
-  //   setIsLoading(false);
-  // }
-
-  // useEffect(() => {
-  //   fetch();
-  // }, [])
-
-  //dummy datas
 
   const transactionProps: Transaction = {
     id: "1",
